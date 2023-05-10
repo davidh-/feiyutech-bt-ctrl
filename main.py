a0447867ec398472b0e64e7a8631808cf0877b84
davidh- <davidhooper4@berkeley.edu>
2023-05-10 00:59:33 -0700

    added keyboard control

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
+import time, keyboard, sys, tty, termios, re ,io
 from gattlib import DiscoveryService, GATTRequester
 
 service = DiscoveryService("hci0")
@@ -67,35 +67,70 @@ def reset_gimbal(req):
     req.write_cmd(handle_write, bytes.fromhex(hex_string_reset))
        
 
+def test_moves(req):
+    # move command 
+    i = 0
+    j = 100
+    while i < j:
+        time.sleep(0.4)
+        if i < j * 0.25:
+            req.write_cmd(handle_write, bytes.fromhex(hex_string_moves[0]))
+        elif i < j * 0.5:
+            req.write_cmd(handle_write, bytes.fromhex(hex_string_moves[1]))
+        elif i < j* 0.75:
+            req.write_cmd(handle_write, bytes.fromhex(hex_string_moves[2]))
+        else:
+            req.write_cmd(handle_write, bytes.fromhex(hex_string_moves[3]))
+        i += 1
+
+class LineBuffer(io.StringIO):
+    def write(self, s):
+        cleaned_output = re.sub(r'(\x1b\[[;\d]*[A-Za-z])', '', s)
+        super().write(cleaned_output)
+
+def keyboard_ctrl(req):
+    # Save the current terminal settings and stdout
+    original_settings = termios.tcgetattr(sys.stdin)
+    original_stdout = sys.stdout
+
+    try:
+        # Disable local echo and redirect stdout
+        tty.setcbreak(sys.stdin.fileno())
+        sys.stdout = LineBuffer()
+
+        while True:
+            key = sys.stdin.read(1)
+            if ord(key) == 27:  # Escape character
+                key += sys.stdin.read(2)
+                if key == '\x1b[A':  # Up arrow key
+                    req.write_cmd(handle_write, bytes.fromhex(hex_string_move_up))
+                elif key == '\x1b[B':  # Down arrow key
+                    req.write_cmd(handle_write, bytes.fromhex(hex_string_move_down))
+                elif key == '\x1b[C':  # Right arrow key
+                    req.write_cmd(handle_write, bytes.fromhex(hex_string_move_right))
+                elif key == '\x1b[D':  # Left arrow key
+                    req.write_cmd(handle_write, bytes.fromhex(hex_string_move_left))
+            elif key == 'r':
+                reset_gimbal(req)
+            elif key == 'q':  # Quit the program
+                break
+
+    finally:
+        # Restore the original terminal settings and stdout
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, original_settings)
+        sys.stdout = original_stdout
+
 def main():
     if find_gimbal():
         
         req = GATTRequester(addr, True)
             
         enable_writes(req)
-        # ~ print("sleep %s sec" % sleep_time)
-        # ~ time.sleep(sleep_time)
+
+        # ~ test_moves(req)
         
-        # ~ reset_gimbal(req)
-        # ~ print("sleep %s sec" % sleep_time)
-        # ~ time.sleep(sleep_time)   
-             
-        # move command 
-        i = 0
-        j = 100
-        while i < j:
-            time.sleep(0.5)
-            if i < j * 0.25:
-                req.write_cmd(handle_write, bytes.fromhex(hex_string_moves[0]))
-            elif i < j * 0.5:
-                req.write_cmd(handle_write, bytes.fromhex(hex_string_moves[1]))
-            elif i < j* 0.75:
-                req.write_cmd(handle_write, bytes.fromhex(hex_string_moves[2]))
-            else:
-                req.write_cmd(handle_write, bytes.fromhex(hex_string_moves[3]))
-            i += 1
+        keyboard_ctrl(req)
             
-        
         print("done")
     else:
         print("Device not found in the discovered devices.")
